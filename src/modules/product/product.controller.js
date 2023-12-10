ad1ba473ec361d58bcb426b011df0ff9f93cbdc0
yous0001 <mad33833@gmail.com>
2023-12-10 02:36:17 +0200

    second commit

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -10,11 +10,33 @@ export const addproduct=async(req,res,next)=>{
             status:409
         })
     }
-    const newpost=await product.create({name,price,description,userID})
+    const newproduct=await product.create({name,price,description,userID})
     res.json({
         message:"added successfully",
         status:200
     })
+}
+
+
+
+
+
+//3st requirement
+export const updateproduct=async(req,res,next)=>{
+    const{_id,loggedInUserID}=req.query
+    const{name,description}=req.body
+
+    const updatedproduct = await product.findOneAndUpdate({_id,userID:loggedInUserID},{name,description},{new:true})
+    if(!updatedproduct){
+        return res.json({
+            message:"update falid"
+            ,status:409
+        })
+    }
+    res.json({
+        message:"update done"
+        ,status:200})
+
 }
 
 //4st requirement
--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -6,5 +6,6 @@ const router=Router();
 router.post("/addproduct",PC.addproduct)
 router.get("/getallproducts",PC.getallproducts)
 router.get("/getallproducts_owners",PC.getallproducts_owners)
+router.put("/updateproduct",PC.updateproduct)
 
 export default router;
